3e28260b54ed3d103cd0a84244d635cff6ecc55b
Dimo <lami4ka@gmail.com>
2018-06-12 18:56:20 -0700

    Fix typing errors in predicate transformers test

--- pyboogie/tests/test_predicate_transformers.py
+++ pyboogie/tests/test_predicate_transformers.py
@@ -8,16 +8,20 @@ from ..bb import Function
 from ..predicate_transformers import wp_stmt, sp_stmt
 from functools import reduce
 from ..z3_embed import TypeEnv_T, getCtx, Int, IntVal, BoolVal, And, Implies
+from ..util import ccast
 import z3
 
+def _toExpr(a: Any) -> z3.ExprRef:
+    return ccast(a, z3.ExprRef)
+
 class TestPredicateTransformers(TestCase):
-    testWPCases : List[Tuple[z3.ExprRef, str, z3.ExprRef, TypeEnv_T]] = [
+    testWPCases : List[Tuple[Any, str, Any, TypeEnv_T]] = [
         (Int('y') == IntVal(4), "x:=y;", Int('x') == IntVal(4), {'x': Int, 'y': Int}),
         (Int('z') == IntVal(4), "x:=y;", Int('z') == IntVal(4), {'x': Int, 'y': Int}),
         (((Int('y') + IntVal(1)) == IntVal(4)), "x:=y+1;", Int('x') == IntVal(4), {'x': Int, 'y': Int}),
         (And(Int('y') == IntVal(4), Int('x') == IntVal(1)), "assert (x==1);", Int('y') == IntVal(4), {'x': Int, 'y': Int}),
-        (Implies(Int('x') == IntVal(1), Int('y') == IntVal(4)), "assume (x==1);", Int('y') == IntVal(4), {'x': Int, 'y': Int}),
-        (Implies(Int('x') == IntVal(1), Int('y') == IntVal(4)), "assume (x==1);", Int('y') == IntVal(4), {'x': Int, 'y': Int}),
+        (Implies(_toExpr(Int('x') == IntVal(1)), _toExpr(Int('y') == IntVal(4))), "assume (x==1);", Int('y') == IntVal(4), {'x': Int, 'y': Int}),
+        (Implies(_toExpr(Int('x') == IntVal(1)), _toExpr(Int('y') == IntVal(4))), "assume (x==1);", Int('y') == IntVal(4), {'x': Int, 'y': Int}),
     ]
 
     def testWPRequireSSA(self):
@@ -54,10 +58,10 @@ class TestPredicateTransformers(TestCase):
             # indices)
             sp_stmt(parseStmt("havoc x;"), BoolVal(True), {'x': Int})
 
-    testSPCases : List[Tuple[z3.ExprRef, str, z3.ExprRef, TypeEnv_T]] = [
+    testSPCases : List[Tuple[Any, str, Any, TypeEnv_T]] = [
         (Int('y') == IntVal(4), "x:=y;", And(Int('x') == Int('y'), Int('y') == IntVal(4)), {'x': Int, 'y': Int}),
         (Int('z') == IntVal(4), "x:=y;", And(Int('x') == Int('y'), Int('z') == IntVal(4)), {'x': Int, 'y': Int, 'z': Int}),
-        (Int('z') == IntVal(4), "x:=y+1;", And(Int('x') == (Int('y')+1), Int('z') == IntVal(4)), {'x': Int, 'y': Int, 'z': Int}),
+        (Int('z') == IntVal(4), "x:=y+1;", And(Int('x') == (Int('y')+IntVal(1)), Int('z') == IntVal(4)), {'x': Int, 'y': Int, 'z': Int}),
         (Int('y') == IntVal(4), "assert (x==1);", And(Int('y') == IntVal(4), IntVal(1) == Int('x')), {'x': Int, 'y': Int}),
         (Int('y') == IntVal(4), "assume (x==1);", And(Int('y') == IntVal(4), IntVal(1) == Int('x')), {'x': Int, 'y': Int}),
     ]
